3399008944cd5863da47b2749e327fc63b93e996
Haofan Fang <haofanfang@gmail.com>
2020-03-07 14:34:42 +0800

    update 永久化数据保存方式之一 SharedPreferences

--- app/src/main/java/com/supremesir/score_java/MainActivity.java
+++ app/src/main/java/com/supremesir/score_java/MainActivity.java
@@ -5,14 +5,19 @@ import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.SavedStateViewModelFactory;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.supremesir.score_java.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final static String KEY_SHP  = "MY_DATA";
+
     ScoreViewModel scoreViewModel;
     ActivityMainBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +28,16 @@ public class MainActivity extends AppCompatActivity {
         scoreViewModel =  new ViewModelProvider(this, new SavedStateViewModelFactory(getApplication(),this)).get(ScoreViewModel.class);
         binding.setData(scoreViewModel);
         binding.setLifecycleOwner(this);
+
+
+//        SharedPreferences sharedPreferences = getPreferences(MODE_PRIVATE);
+        SharedPreferences sharedPreferences = getSharedPreferences("MY_DATA", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putInt(KEY_SHP, 1000);
+        // 使用异步的apply而不是commit，保证并发操作数据安全
+//        editor.commit();
+        editor.apply();
+        int temp = sharedPreferences.getInt(KEY_SHP, 0);
+        Log.d(KEY_SHP, "SharedPreferences Value is : " + temp);
     }
 }
